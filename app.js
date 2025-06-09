68380ba9a2fb9ffedd4c418bea0fd867284b7f93
Saniya Ahmad <saniyanitpatna@gmail.com>
2025-06-09 23:25:13 +0530

    final commit

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ async function main(){
 const store=MongoStore.create({
     mongoUrl:dbUrl,
     crypto:{
-        secret:'mysupersecretcode'
+        secret:process.env.SECRET
     },
     touchAfter:24*60*60,
 })
@@ -57,7 +57,7 @@ store.on('error',()=>{
 })
 const sessionOptions={
     store,
-    secret:'mysupersecretcode',
+    secret:process.env.SECRET,
     resave:false,
     saveUninitialized:true,
     cookie:{
